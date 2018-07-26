95ff288db7f054db66533271453f7bd2d5159580
elizabeth-smt <Luiza17061998@gmail.com>
2018-07-26 09:56:17 +0300

    weird

--- app/js/main.js
+++ app/js/main.js
@@ -18,7 +18,6 @@ $('.person-carousel')
         slidesToShow: 5,
         slidesToScroll: 1,
         asNavFor: '.review-carousel',
-        // dots: true,
         arrows: true,
         centerMode: true,
         focusOnSelect: true,
@@ -28,23 +27,6 @@ $('[data-fancybox="gallery"]').fancybox({
 	// Options will go here
 });
 
-$(function() {
-    var pull 	= $('.nav__menu');
-    menu 		= $('.nav__link');
-    menuHeight	= menu.height();
-
-    $(pull).on('click', function(e) {
-        e.preventDefault();
-        menu.slideToggle();
-    });
-
-    $(window).resize(function(){
-        var w = $(window).width();
-        if(w > 320 && menu.is(':hidden')) {
-            menu.removeAttr('style');
-        }
-    });
-});
 
 $('.menu-btn').on('click', function(){
     $(this).toggleClass('is-open');
